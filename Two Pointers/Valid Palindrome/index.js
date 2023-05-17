dd26949344583928945c23de82bb5f0d661711c3
aashish-moktan <aashiislama@gmail.com>
2023-05-17 18:17:35 +0545

    Solve valid palindrome problem

---
+++ Two Pointers/Valid Palindrome/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} string
+ * @return {boolean}
+ */
+
+var isPalindrome = function (s) {
+  const replacedString = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  let i = 0;
+  let j = replacedString.length - 1;
+  while (i <= j) {
+    if (replacedString[i] !== replacedString[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
+};
+
+console.log({ isValid: isPalindrome("A man, ama") });
